a09915eb4862f597f12371be2ba8ec0c37271f04
muhammad talal <talalilyas11@gmail.com>
2026-02-03 15:53:29 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { GoogleLogin } from "@react-oauth/google";
-
-import { useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
 export default function App() {
+    const [user,setuser] = useState(null)
+ 
+    const handleSuccess = (credentialResponse) => {
+   
+    const decoded = jwtDecode(credentialResponse.credential);
+    console.log(decoded);
+    setuser(decoded); 
+  };
   const handleError = () => {
     console.log("Login Failed");
   };
@@ -10,9 +18,22 @@ export default function App() {
     <div style={{ padding: "50px", textAlign: "center" }}>
       <h1>React Google Auth</h1>
 
-      <GoogleLogin onError={handleError} useOneTap />
+     {!user ? (
+        <GoogleLogin 
+          onSuccess={handleSuccess} 
+          onError={handleError} 
+          useOneTap 
+        />
+      ) : (
+        <div>
+         
+          <h3>Welcome, {user.name}</h3>
+          <p>{user.email}</p>
+          <button onClick={() => setUser(null)}>Logout</button>
+        </div>
+      )}
 
-      <div></div>
+      <div> </div>
     </div>
   );
 }
