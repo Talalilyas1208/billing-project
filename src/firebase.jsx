95e235f102da61ae4fabe825f780e14ea91719c5
muhammad talal <talalilyas11@gmail.com>
2026-02-06 13:56:15 +0500

    changes

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import this
 
 const firebaseConfig = { 
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDtFQZDzTBwP5qRVgcC2ZGTD-yIEjsgEfg",
   authDomain: "recipet-texture.firebaseapp.com",
   projectId: "recipet-texture",
   storageBucket: "recipet-texture.firebasestorage.app",
   messagingSenderId: "749725618193",
   appId: "1:749725618193:web:cfceee84c0d63df2b721b0"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app); // Export this
