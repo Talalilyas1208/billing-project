7aa9b622dbe5db96928f8c0d36824bd79e980254
muhammad talal <talalilyas11@gmail.com>
2026-02-14 01:50:22 +0500

    improvements

--- src/firebases.jsx
+++
@@ -1,5 +0,0 @@
-// Inside firebase.js
-import { getFirestore } from "firebase/firestore";
-
-// ... your existing config and app initialization
-export const db = getFirestore(app); // Add and export this
