326650957b52d23363e3be86e175a837fa0e0fc5
muhammad talal <talalilyas11@gmail.com>
2026-02-23 23:04:13 +0500

    improvments

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,6 +9,7 @@ export default function Button(props) {
     signout: "bg-transparent border-2 border-red-600 text-red-600 hover:bg-red-50",
     facebook:"bg-white border border-gray-300 text-gray-700 hover:bg-gray-50 flex items-center justify-center gap-",
     default: "bg-green-600 hover:bg-green-700 text-white",
+    sginout : "bg-red-50 text-red-600 border border-red-200 p-2 w-full rounded-lg hover:bg-red-600 hover:text-white transition font-medium"
   };
   const variantClass = variants[variant] || variants.default;
   return (
