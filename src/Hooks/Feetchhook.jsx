e641c9911afae1614e080c15493560a387074374
muhammad talal <talalilyas11@gmail.com>
2026-02-25 16:24:10 +0500

    improvements in fetch componenets

--- src/Hooks/Feetchhook.jsx
+++ src/Hooks/Feetchhook.jsx
@@ -1,12 +1,40 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
-export default function Feetchhooks() {
+export default function Feetchhooks(url) {
+  const [data, setdata] = useState("");
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    let ismount = true ;
+    const hookfetch = async () => {
+      setloading(true);
+      try {
+        const req = await fetch(url);
+
+        const data = await req.json();
+
+        console.log(data);
+        if (ismount) {
+          setdata(result);
+          setError(null);
+        }
+      } catch (error) {
+        if (ismount) {
+          setError(err.message);
+        }
+      } finally {
+        if (ismount) {
+          setloading(false);
+        }
+      }
     
-  
-     
-        
-       const [data,setdata] = useState("")
-      
-
-
+    };
+      hookfetch()
+       return () => {
+      ismount = false;
+    };
+  },[url]);
+  return { data, loading, error };
 }
