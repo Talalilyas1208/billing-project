79c1e70b4a92ccf49755b90075b31c8fb3004507
muhammad talal <talalilyas11@gmail.com>
2026-03-16 14:58:11 +0500

    improvements

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,9 +1,14 @@
+ import Button from "../components/Button"
+import EmptyInvoicePage from "./Emptyinvoicepage"
+ 
+
 export default function Invoice () {
-
-
 
     return(
         <>
+       
+ <EmptyInvoicePage/>
+
         </>
     )
 }
---
+++ src/pages/Newinvoice.jsx
@@ -0,0 +1,6 @@
+export default function Newinvoice(){
+    return(
+        <></>
+    )
+
+}
