437730e98d04b3d5822a3d271f54c4c23a634e6e
muhammad talal <talalilyas11@gmail.com>
2026-04-03 17:51:24 +0500

    improvements

--- src/pages/Newinvoice.jsx
+++ src/pages/Newinvoice.jsx
@@ -1,6 +1,21 @@
+import Table from "../components/Table";
+import Input from "../components/Input";
+import { useState } from "react";
 export default function Newinvoice(){
+
+const [state,setstate] =useState()
+
+
+ 
     return(
-        <></>
+        <>
+         <Table>
+          <Input >
+          </Input>
+
+
+         </Table>
+        </>
     )
 
 }
