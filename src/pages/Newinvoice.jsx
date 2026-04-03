27dadf84bd83d28e25f4326dc02f277e020ccfa8
muhammad talal <talalilyas11@gmail.com>
2026-04-04 00:14:32 +0500

    improvements

--- src/pages/Newinvoice.jsx
+++ src/pages/Newinvoice.jsx
@@ -1,21 +1,24 @@
 import Table from "../components/Table";
 import Input from "../components/Input";
 import { useState } from "react";
-export default function Newinvoice(){
+import { Row, Col, Layout } from "antd";
+import { Card } from "antd";
+import CardComponent from "../components/CardComponent";
+export default function Newinvoice() {
+  const [state, setstate] = useState();
 
-const [state,setstate] =useState()
+  const { Header } = Layout;
 
-
- 
-    return(
-        <>
-         <Table>
-          <Input >
-          </Input>
-
-
-         </Table>
-        </>
-    )
-
+  return (
+    <>
+      <Row>
+        <CardComponent style={{width:1300}}>
+          <Header> </Header>
+          <Col span={4}>
+            <Input antUI={{ size: "large" }} />
+          </Col>
+        </CardComponent>
+      </Row>
+    </>
+  );
 }
