b7f093864a9408d0324065cf333221dc49af4bd9
muhammad talal <talalilyas11@gmail.com>
2026-04-08 14:10:13 +0500

    improvements

--- src/pages/Newinvoice.jsx
+++ src/pages/Newinvoice.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Row, Col, Layout } from "antd";
 import { Card } from "antd";
 import CardComponent from "../components/CardComponent";
+import Config from "../components/Config";
 export default function Newinvoice() {
   const [state, setstate] = useState();
 
@@ -11,28 +12,21 @@ export default function Newinvoice() {
 
   return (
     <>
+    <Config>
       <Row gutter={16}>
+
         <CardComponent style={{width:1300}}>
           <Header> Create invoice </Header>
           <Col span={4}>
             <Input antUI={{ size: "large" }} />
           </Col>
-           <Col span={4}>
-            <Input antUI={{ size: "large" }} />
-          </Col> <Col span={4}>
-            <Input antUI={{ size: "large" }} />
-          </Col>
-           <Col span={4}>
-            <Input antUI={{ size: "large" }} />
-          </Col>
-           <Col span={4}>
-            <Input antUI={{ size: "large" }} />
-          </Col>
+           
            <Col span={4}>
             <Input antUI={{ size: "large" }} />
           </Col>
         </CardComponent>
       </Row>
+      </Config>
     </>
   );
 }
