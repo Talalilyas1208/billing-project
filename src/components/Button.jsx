ed9642ec7904fd737a8ac9ec5a300ec1fccb87a7
muhammad talal <talalilyas11@gmail.com>
2026-05-06 12:55:59 +0500

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,24 +11,13 @@ export default function Button(props) {
   } = props;
 
   const variants = {
-    signout:
-      "bg-red-50 text-red-600 border border-red-200 hover:bg-red-600 hover:text-white transition font-medium",
-    nav: "bg-transparent text-gray-700 hover:bg-neutral-200 justify-start font-medium pl-4 pr-35 ",
-
-    profile:
-      "bg-white border border-neutral-200 text-gray-700 hover:bg-neutral-50 shadow-sm",
-
-    default: "bg-green-600 text-white",
-
-    product: "bg-black text-white py-3 px-6 rounded",
-
-    modal: "bg-transparent text-gray-500 hover:bg-gray-100 p-2 rounded-md",
+    default: "bg-black-600 text-white",
     addbutton: "bg-transparent hover:bg-gray-100 text-gray  rounded-md p-2",
   };
   const variantClass = variants[variant] || variants.default;
 
   return (
-    <button
+    <Button
       className={`p-2.5 rounded-lg transition active:scale-[0.98] disabled:opacity-50 flex items-center justify-center ${variantClass} ${className} ${antUI}`} 
       type={type || "button"}
       onClick={onClick}
@@ -37,6 +26,6 @@ export default function Button(props) {
  
     >
       {children}
-    </button>
+    </Button>
   );
 }
