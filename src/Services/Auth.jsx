0af984e140361e33e968b40cb0125c6f50245226
muhammad talal <talalilyas11@gmail.com>
2026-05-19 12:23:14 +0500

    getting token from the firebase

--- src/Services/Auth.jsx
+++ src/Services/Auth.jsx
@@ -10,23 +10,24 @@ import {
 } from "firebase/auth";
 import { auth } from "../firebase/firebase";
 
-const User = (user) => ({
+const User = (user, token) => ({
   uid: user.uid,
   email: user.email,
   displayName: user.displayName || "User",
-  metadata: user.metadata
-
+  metadata: user.metadata,
+  token: token 
 });
 
 export const loginWithEmail = async (email, password) => {
   const data = await signInWithEmailAndPassword(auth, email, password);
-  
-  return User(data.user);
+  const token = await data.user.getIdToken();
+  return User(data.user, token);
 };
 
 export const registerWithEmail = async (email, password, displayName) => {
   const data = await createUserWithEmailAndPassword(auth, email, password);
   await updateProfile(data.user, { displayName });
+  await data.user.getIdToken(); 
   await sendEmailVerification(data.user);
   return data.user;
 };
@@ -36,10 +37,10 @@ export const loginWithSocial = async (type) => {
     type === "google" ? new GoogleAuthProvider() : new FacebookAuthProvider();
 
   const data = await signInWithPopup(auth, provider);
+  const token = await data.user.getIdToken();
 
-  return User(data.user);
+  return User(data.user, token);
 };
-
 
 export const logoutUser = async () => {
   await signOut(auth);
