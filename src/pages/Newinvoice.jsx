0d237903379b264b723f9ee2846b123f8a0c7324
muhammad talal <talalilyas11@gmail.com>
2026-06-02 16:33:51 +0500

    improvements

--- src/pages/Newinvoice.jsx
+++ src/pages/Newinvoice.jsx
@@ -5,41 +5,72 @@ import { Row, Col, Layout } from "antd";
 import { Card } from "antd";
 import CardComponent from "../components/CardComponent";
 import Config from "../components/Config";
+
 export default function Newinvoice() {
-  const [state, setstate] = useState();
-
-
   const { Header } = Layout;
 
   return (
     <>
       <Config>
-        <Row gutter={16}>
-          <CardComponent
-            style={{
-              width: 1300,
-              borderRadius: "20px",
-              borderColor: "#c7c2c2ff",
-            }}
+        <CardComponent
+          style={{
+            width: 1300,
+            borderRadius: "20px",
+            borderColor: "#c7c2c2ff",
+          }}
+        >
+          <Header>Create Invoice</Header>
+
+          <Row
+            justify="space-between"
+            style={{ marginTop: "10px", padding: "0 16px" }}
           >
-            <Header> Create invoice </Header>
-            <Col span={4} style={{}}>
-              <Input antUI={{ size: "large" }} style={{ marginTop: "10px" }} />
-            </Col>
             <Col span={4}>
-              <Input antUI={{ size: "large" }} style={{ marginTop: "10px" }} />
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px" }}
+              />
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px" }}
+              />
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px" }}
+              />
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px" }}
+              />
             </Col>
-            <Col span={4}>
-              <Input antUI={{ size: "large" }} style={{ marginTop: "10px" }} />
+
+            <Col
+              span={5}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-end",
+              }}
+            >
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px" }}
+              />
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px", }}
+              />
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px"}}
+              />
+              <Input
+                antUI={{ size: "large" }}
+                style={{ marginBottom: "16px", width: "100%" }}
+              />
             </Col>
-            <Col span={4} offset={20}>
-              <Input antUI={{ size: "large" }} style={{ marginTop: "10px" }} />
-            </Col>
-             <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 4 }}>
-       <Input antUI={{ size: "large" }} style={{ marginTop: "10px" }} />
-    </Col>
-          </CardComponent>
-        </Row>
+          </Row>
+        </CardComponent>
       </Config>
     </>
   );
