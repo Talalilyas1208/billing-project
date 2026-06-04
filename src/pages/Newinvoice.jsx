d74d41c4353e5fac8487ba7db996de4c6ccd8e68
muhammad talal <talalilyas11@gmail.com>
2026-06-04 13:52:01 +0500

    updates in invoice componenents

--- src/pages/Newinvoice.jsx
+++ src/pages/Newinvoice.jsx
@@ -1,12 +1,27 @@
 import Table from "../components/Table";
 import Input from "../components/Input";
 import { useState } from "react";
-import { Row, Col, Layout } from "antd";
+import { Row, Col, Layout,Select,Divider,Space,Button } from "antd";
+import { PlusOutlined } from '@ant-design/icons';
+import { useRef } from "react";
 import { Card } from "antd";
 import CardComponent from "../components/CardComponent";
 import Config from "../components/Config";
 
 export default function Newinvoice() {
+   const [items, setItems] = useState(['jack', 'lucy']);
+  const [name, setName] = useState('');
+  const inputRef = useRef(null);
+  const onNameChange = event => {
+    setName(event.target.value);
+  };
+  const addItem = e => {
+    e.preventDefault();
+    setItems([...items, name || `New item`]);
+    setName('');
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);}
   const { Header } = Layout;
 
   return (
@@ -26,6 +41,29 @@ export default function Newinvoice() {
             style={{ marginTop: "10px", padding: "0 16px" }}
           >
             <Col span={4}>
+              <Select
+      style={{ width: 300 }}
+      placeholder="custom dropdown render"
+      popupRender={menu => (
+        <>
+          {menu}
+          <Divider style={{ margin: '8px 0' }} />
+          <Space style={{ padding: '0 8px 4px' }}>
+            <Input
+              placeholder="Please enter item"
+              ref={inputRef}
+              value={name}
+              onChange={onNameChange}
+              onKeyDown={e => e.stopPropagation()}
+            />
+            <Button type="text" icon={<PlusOutlined />} onClick={addItem}>
+              Add item
+            </Button>
+          </Space>
+        </>
+      )}
+      options={items.map(item => ({ label: item, value: item }))}
+    />
               <Input
                 antUI={{ size: "large" }}
                 style={{ marginBottom: "16px" }}
