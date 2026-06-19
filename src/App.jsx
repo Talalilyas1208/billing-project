57a0c3b6c7efabe56a51fb8cfa176a8a58b4ea9b
muhammad talal <talalilyas11@gmail.com>
2026-06-19 17:03:34 +0500

    improvemnets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashborad";
-import PublicRoute from "./routes/Publicroutes"
-import PrivateRoute from "./Routes/Privateroutes";
-
+import PublicRoute from "./routes/Publicroutes";
+import PrivateRoute from "./routes/Privateroutes";
 import Register from "./Register/Register";
-import Login from "./Login/login";
+import Login from "./login/Login";
 import Produts from "./pages/Products";
 import Invoice from "./pages/Invoice";
 import EmptyInvoicePage from "./pages/Emptyinvoicepage";
@@ -24,12 +23,11 @@ export default function App() {
           <Route path="/dashboard" element={<Dashboard />}>
             <Route path="products" element={<Produts />} />
             <Route path="invoices" element={<Invoice />}>
-            <Route index element={<EmptyInvoicePage />} /> 
-            <Route path="new" element={<Newinvoice />} />
-          
+              <Route index element={<EmptyInvoicePage />} />
+              <Route path="new" element={<Newinvoice />} />
             </Route>
-            <Route path="offers" element={<Offers/>} />
-            <Route path="contact" element={<Contact/>}/>
+            <Route path="offers" element={<Offers />} />
+            <Route path="contact" element={<Contact />} />
           </Route>
         </Route>
         <Route path="*" element={<Navigate to="/login" />} />
