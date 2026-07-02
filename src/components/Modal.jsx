b00aebef8326bfa14cfa99c68d96373c94566ebb
muhammad talal <talalilyas11@gmail.com>
2026-07-02 13:57:00 +0500

    improvements in code and fixes some issue

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,33 +1,9 @@
 import { Modal } from "antd";
-import { App } from 'antd';
-const { confirm } = Modal;
 
 function Modals(props) {
-  const { isOpen, onClose, children, style, form } = props;
+  const { isOpen, children, style, alert } = props;
   const { width, top, title, height } = style;
-   const { modal } = App.useApp(); 
-  const handleCloseAndReset = () => {
-    onClose();
-    if (form) {
-      form.resetFields();
-    }};
-  const alert = () => {
-    if (!form?.isFieldsTouched()) {
-      handleCloseAndReset();
-      return;}
-    modal.confirm({
-      title: "Confirm navigation",
-      style: { top: 300 },
-      content:"Your changes have not been saved yet. Are you sure you want to leave this page?",
-      okText: "Leave this page",
-      okType: "danger",
-      cancelText: "No, stay",
-      width: "40%",
-      onOk() {
-        handleCloseAndReset();
-      },
-    });
-  };
+
   return (
     <Modal
       title={title}
@@ -36,7 +12,8 @@ function Modals(props) {
       onCancel={alert}
       footer={null}
       width={width}
-      style={{ top }}>
+      style={{ top }}
+    >
       {children}
     </Modal>
   );
