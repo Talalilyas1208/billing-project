9c72d3b8dbc060fb3fc5b79ab022a157ec203e6c
muhammad talal <talalilyas11@gmail.com>
2026-07-06 17:06:16 +0500

    improvements in code

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,24 @@
 import { Modal } from "antd";
 
 function Modals(props) {
-  const { isOpen, children, style, alert } = props;
+  const { isOpen, children, style, alert} = props;
   const { width, top, title, height } = style;
 
   return (
     <Modal
       title={title}
       open={isOpen}
+  styles={{ 
+    body: { 
+      maxHeight: '60vh', 
+      overflowY: 'auto' 
+    } 
+  }}
       height={height}
       onCancel={alert}
       footer={null}
       width={width}
+     
       style={{ top }}
     >
       {children}
