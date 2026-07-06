1fddd2f5be47749f891adf4dbe118cb07e73427d
muhammad talal <talalilyas11@gmail.com>
2026-07-06 13:23:19 +0500

    improvements in code

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,8 +2,6 @@ import { Table as AntTable } from "antd";
 import Config from "./Config";
 
 export default function Table({ data, columns, loading, style, pagination }) {
-
-
   return (
     <Config>
        {!data ? (
