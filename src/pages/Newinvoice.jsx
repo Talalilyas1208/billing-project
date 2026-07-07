a1a6ec7ab8c9ceca22646f5b413a0f76695e7a87
muhammad talal <talalilyas11@gmail.com>
2026-07-07 14:34:17 +0500

    logical change at a time

--- src/pages/Newinvoice.jsx
+++ src/pages/Newinvoice.jsx
@@ -17,7 +17,8 @@ export default function Newinvoice() {
   const navigate = useNavigate();
   const [form] = Form.useForm();
   const { modal } = App.useApp();
-  const   handleOpen = () => {
+
+  const handleOpen = () => {
     setIsOpen(true);
     setSelectOpen(false);
   };
@@ -47,126 +48,170 @@ export default function Newinvoice() {
   const handleclick = () => {
     navigate("/dashboard/invoices");
   };
+
   return (
     <>
       <Config>
-        <Row>
-          <Col span={2}>
+        <Row
+          gutter={[6, 6]}
+          justify="space-between"
+          align="middle"
+          style={{ padding: "0 8px" }}
+        >
+          <Col xs={24} sm={4} md={2} lg={2}>
             <Button
               type="text"
               icon={<LeftOutlined />}
               antUI={{ size: "large" }}
               style={{ backgroundColor: "#fefefe" }}
-              onClick={handleclick}/>
+              onClick={handleclick}
+            />
           </Col>
-          <Col span={2} offset={11}>
-            <Button
-              type="primary"
-              antUI={{ size: "large", shape: "round" }}
-              style={{ backgroundColor: "#000", color: "#fff" }}
-              className="py-3 px-8">
-              Approve and send
-            </Button>
-          </Col>
-          <Col span={2} offset={1}>
-            <Button
-              type="outlined"
-              antUI={{ size: "large", shape: "round" }}
-              style={{
-                backgroundColor: "#f8f8f8ff",
-                color: "#000000ff",
-                borderColor: "#d0ceceff",
-                colorPrimaryHover: "#40a9ff",
-              }}
-              className="py-3 px-8"
-            >
-              Approve and send
-            </Button>
-          </Col>
-          <Col span={2} offset={1}>
-            <Button
-              antUI={{ size: "large", shape: "round", color: "black" }}
-              style={{
-                backgroundColor: "#f8f8f8ff",
-                color: "#080808ff",
-                borderColor: "#d0ceceff",
-              }}
-            >
-              Approve and send
-            </Button>
+
+          <Col xs={24} sm={24} md={18} lg={10}>
+            <Row gutter={[6, 6]} justify="end" wrap>
+              <Col xs={24} sm={8} md={8} lg={8}>
+                <Button
+                  block
+                  antUI={{ size: "large", shape: "round", color: "black" }}
+                  style={{
+                    backgroundColor: "#f8f8f8ff",
+                    color: "#080808ff",
+                    borderColor: "#d0ceceff",
+                  }}
+                >
+                  Approve and send
+                </Button>
+              </Col>
+              <Col xs={24} sm={8} md={8} lg={8}>
+                <Button
+                  block
+                  type="outlined"
+                  antUI={{ size: "large", shape: "round" }}
+                  style={{
+                    backgroundColor: "#f8f8f8ff",
+                    color: "#000000ff",
+                    borderColor: "#d0ceceff",
+                    colorPrimaryHover: "#40a9ff",
+                  }}
+                >
+                  Approve and send
+                </Button>
+              </Col>
+              <Col xs={24} sm={8} md={8} lg={8}>
+                <Button
+                  block
+                  type="primary"
+                  antUI={{ size: "large", shape: "round" }}
+                  style={{ backgroundColor: "#000", color: "#fff" }}
+                  className="py-3 px-8"
+                >
+                  Approve and send
+                </Button>
+              </Col>
+            </Row>
           </Col>
         </Row>
+
         <Modals
           isOpen={isOpen}
           form={form}
           alert={alert}
-          style={{ width: 840, top: 170, title: "Create Contact" }}>
+          style={{ width: 840, top: 170, title: "Create Contact" }}
+
+          centered
+        >
           <Newcustomers form={form} />
         </Modals>
-        <CardComponent
-          style={{
-            width: 1400,
-            borderRadius: "10px",
-            marginTop: "10px",
-            borderColor: "#b9adadff",
-          }}>
-          <h1>create invoice</h1>
-          <Row
-            justify="space-between"
-            style={{ marginTop: "10px", padding: "0 16px" }} >
-            <Col span={4}>
-              <Select
-                placeholder="Select customer"
-                open={selectOpen}
-                onOpenChange={setSelectOpen}
-                popupRender={(menu) => (
-                  <>
-                    {menu}
-                    <Divider style={{ margin: "8px" }} />
-                    <Space style={{ padding: "0 29px 4px" }}>
-                      <Button
-                        type="text"
-                        align="center"
-                        icon={<PlusOutlined />}
-                        onMouseDown={(e) => e.preventDefault()}
-                        onClick={handleOpen}
-                      >
-                        create new
-                      </Button>
-                    </Space>
-                  </>
-                )}
-              />
-              <Input
-                size="large"
-                style={{ marginBottom: "16px", marginTop: "5px" }}
-              />
-              <Input size="large" style={{ marginBottom: "16px" }} />
-            </Col>
-            <Col
+
+        <Row gutter={[10, 10]}>
+          <Col span={24}>
+            <CardComponent
               style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-end",
+                width: "100%",
+                maxWidth: 1500,
+                borderRadius: "10px",
+                marginTop: "10px",
+                borderColor: "#b9adadff",
               }}
             >
-              <Col>
-                <Input
-                  antUI={{ size: "large" }}
-                  style={{ marginBottom: "16px" }}
-                />
-              </Col>
-              <Input
-                antUI={{ size: "large" }}
-                style={{ marginBottom: "16px" }}
-              />
-              <Input
-                antUI={{ size: "large" }}
-                style={{ marginBottom: "16px", width: "100%" }}
-              />
-            </Col>
-          </Row>
-        </CardComponent>
+              <h1>create invoice</h1>
+              <Row
+                gutter={[16, 0]}
+                justify="space-between"
+                style={{ marginTop: "10px", padding: "0 14px" }}
+              >
+                <Col xs={24} sm={24} md={12} lg={5}>
+                  <Select
+                    style={{ width: "100%" }}
+                    antUI={{ size: "large" }}
+                    placeholder="Select customer"
+                    open={selectOpen}
+                    onOpenChange={setSelectOpen}
+                    popupRender={(menu) => (
+                      <>
+                        {menu}
+                        <Divider style={{ margin: "8px" }} />
+                        <Space>
+                          <Row justify="end">
+                            <Col span={10}>
+                              <Button
+                                type="text"
+                                align="center"
+                                icon={<PlusOutlined />}
+                                onMouseDown={(e) => e.preventDefault()}
+                                onClick={handleOpen}
+                              >
+                                create new
+                              </Button>
+                            </Col>
+                          </Row>
+                        </Space>
+                      </>
+                    )}
+                  />
+                  <Input
+                    antUI={{ size: "large" }}
+                    style={{
+                      width: "100%",
+                      marginBottom: "16px",
+                      marginTop: "5px",
+                    }}
+                  />
+                  <Input
+                    antUI={{ size: "large" }}
+                    style={{ width: "100%", marginBottom: "16px" }}
+                  />
+                </Col>
+
+                <Col
+                  xs={24}
+                  sm={24}
+                  md={12}
+                  lg={5}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "stretch",
+                  }}
+                >
+                  <Input
+                    antUI={{ size: "large" }}
+                    style={{ width: "100%", marginBottom: "16px" }}
+                  />
+                  <Input
+                    antUI={{ size: "large" }}
+                    style={{ width: "100%", marginBottom: "16px" }}
+                  />
+                  <Input
+                    antUI={{ size: "large" }}
+                    style={{ width: "100%", marginBottom: "16px" }}
+                  />
+                </Col>
+              </Row>
+            </CardComponent>
+          </Col>
+        </Row>
       </Config>
     </>
   );
