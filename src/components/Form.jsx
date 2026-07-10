df7abdbd021c3dea7594a185dfbce49cb637c9dc
muhammad talal <talalilyas11@gmail.com>
2026-07-10 17:51:56 +0500

    remove the extra code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,62 @@
-export default function Form() {
+import React from 'react';
+import { Form, DatePicker, Space } from 'antd';
+import InputTextAreas from './InputTextAreas';
+import Input from './Input';
+import Select from './Select';
+import Button from './Button';
+
+const ReusableForm = ({ config, onSubmit, submitText = 'Submit' }) => {
+  const [form] = Form.useForm();
 
 
-    return (
-        <>
-        </>
-    )
-}
+  const handleFinish = (values) => {
+    onSubmit(values);
+  };
+
+
+  const renderField = (field) => {
+    switch (field.type) {
+      case 'select':
+        return (
+          <Select placeholder={field.placeholder}>
+            {field.options?.map((opt) => (
+              <Select.Option key={opt.value} value={opt.value}>
+                {opt.label}
+              </Select.Option>
+            ))}
+          </Select>
+        );
+      case 'datepicker':
+        return <DatePicker style={{ width: '100%' }} />;
+      case 'textarea':
+        return <InputTextAreas placeholder={field.placeholder} />;
+  
+      default:
+        return <Input placeholder={field.placeholder} />;
+    }
+  };
+
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      onFinish={handleFinish}
+      style={{ maxWidth: '600px', margin: '0 auto' }}
+    >
+      {config.map((field) => (
+        <Form.Item
+          key={field.name}
+          label={field.label}
+          name={field.name}
+          rules={field.rules || []}
+        >
+          {renderField(field)}
+        </Form.Item>
+      ))}
+
+    
+    </Form>
+  );
+};
+
+export default ReusableForm;
