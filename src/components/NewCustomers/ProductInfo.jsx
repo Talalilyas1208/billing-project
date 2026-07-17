d056512fa46db97d3771435b1e91baed5bbfe1b0
muhammad talal <talalilyas11@gmail.com>
2026-07-17 17:56:45 +0500

    improvements

--- src/components/NewCustomers/ProductInfo.jsx
+++ src/components/NewCustomers/ProductInfo.jsx
@@ -1,4 +1,5 @@
 import { Form } from "antd";
+
 import Input from "../Input";
 import Numbersinput from "../Numbersinput";
 
@@ -8,10 +9,17 @@ export default function ProductInfo() {
       <Form.Item
         name="price"
         label="Price"
-        rules={[{ required: true }]}
+        rules={[
+          {
+            required: true,
+          },
+        ]}
       >
         <Numbersinput
-          antUI={{ size: "large", precision: 2 }}
+          antUI={{
+            size: "large",
+            precision: 2,
+          }}
         />
       </Form.Item>
 
@@ -25,14 +33,19 @@ export default function ProductInfo() {
           },
         ]}
       >
-        <Input antUI={{ size: "large" }} />
+        <Input
+          antUI={{
+            size: "large",
+          }}
+        />
       </Form.Item>
 
-      <Form.Item
-        name="supplier"
-        label="Supplier Product Number"
-      >
-        <Input antUI={{ size: "large" }} />
+      <Form.Item name="supplier" label="Supplier Product Number">
+        <Input
+          antUI={{
+            size: "large",
+          }}
+        />
       </Form.Item>
     </>
   );
