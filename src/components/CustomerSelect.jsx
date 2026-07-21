ee55314f37ce07bc9e6593d2782f3971eaaea9dd
muhammad talal <talalilyas11@gmail.com>
2026-07-22 00:20:33 +0500

    improvements

--- src/components/CustomerSelect.jsx
+++ src/components/CustomerSelect.jsx
@@ -1,34 +1,36 @@
 import { Row, Col, Divider } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+
 import Select from "./Select";
-import { useEffect, useState } from "react";
 import Button from "./Button";
-import useFetch from "../hooks/Usefetch";
-export default function CustomerSelect({ open, onOpenChange, onCreateNew }) {
-  const [customerstest , setcustomer ] =useState([])
-  const {data : customerss } = useFetch("/api/Customer")
-useEffect(() => {
-  if (Array.isArray(customerss?.data)) {
-    setcustomer(
-      customerss.data.map((item) => ({
-        value: item.id ?? item._id,   
-        label: item.Company_name,    
-      })),
-    );
-  }
-}, [customerss]);
+
+export default function CustomerSelect({
+  open,
+  onOpenChange,
+  onCreateNew,
+  customers = [],
+  loading,
+}) {
+  const customerOptions = customers.map((item) => ({
+    value: item.id ?? item._id,
+    label: item.Company_name,
+  }));
+
   return (
     <Select
       style={{ width: "100%" }}
       antUI={{ size: "large" }}
       placeholder="Select customer"
       open={open}
-      options={customerstest}
+      loading={loading}
+      options={customerOptions}
       onOpenChange={onOpenChange}
       popupRender={(menu) => (
         <>
           {menu}
+
           <Divider style={{ margin: "8px" }} />
+
           <Row justify="end" style={{ padding: "0 8px" }}>
             <Col>
               <Button
@@ -37,7 +39,7 @@ useEffect(() => {
                 onMouseDown={(e) => e.preventDefault()}
                 onClick={onCreateNew}
               >
-                create new
+                Create New
               </Button>
             </Col>
           </Row>
