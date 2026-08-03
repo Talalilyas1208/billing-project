1ce57a84a16e0c02170fc61087c8a6bd4b8695df
muhammad talal <talalilyas11@gmail.com>
2026-08-03 18:13:05 +0500

    feat:updated version of pnpm

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { api } from './apiSlice';
+
 
 export const store = configureStore({
   reducer: {
